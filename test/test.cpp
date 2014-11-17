03ce5e9dd514283ba4f2b3585ffcef90a06c02f9
loczaj <loczaj@gmail.com>
2014-11-17 16:04:06 +0000

    Right order of variables in EXPECT_EQ

--- test/test.cpp
+++ test/test.cpp
@@ -4,9 +4,8 @@
 TEST(BinaryChearch, FindExistingValue) {
 	blitz::Array<double, 1> ar(5);
 	ar = 1.0, 2.0, 3.0, 4.0, 5.0;
-	EXPECT_EQ(binary_search(ar, 1.0), 0);
-	EXPECT_EQ(binary_search(ar, 2.0), 1);
-	EXPECT_EQ(binary_search(ar, 3.0), 2);
-	EXPECT_EQ(binary_search(ar, 5.0), 4);
+	EXPECT_EQ(0, binary_search(ar, 1.0));
+	EXPECT_EQ(1, binary_search(ar, 2.0));
+	EXPECT_EQ(2, binary_search(ar, 3.0));
+	EXPECT_EQ(4, binary_search(ar, 5.0));
 }
-
