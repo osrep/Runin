d0029ab99368379232f93a2e376a7a02514a1344
loczaj <loczaj@gmail.com>
2014-11-17 17:10:34 +0000

    Test and fix equal() function

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,14 @@
 #include <gtest/gtest.h>
 #include "../cpo_utils.h"
 
-TEST(BinaryChearch, FindExistingValue) {
+TEST(Equal, Tolerance) {
+	EXPECT_TRUE(equal(1.0, 1.1, 0.1));
+	EXPECT_TRUE(equal(-10.0, -11.0, 0.1));
+	EXPECT_FALSE(equal(1.0, 1.2, 0.1));
+	EXPECT_FALSE(equal(-10.0, -12.0, 0.1));
+}
+
+TEST(BinarySearch, FindExistingValue) {
 	blitz::Array<double, 1> ar(10);
 	ar = 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0;
 	EXPECT_EQ(0, binary_search(ar, 0.0));
@@ -14,4 +21,18 @@ TEST(BinaryChearch, FindExistingValue) {
 	EXPECT_EQ(7, binary_search(ar, 7.0));
 	EXPECT_EQ(8, binary_search(ar, 8.0));
 	EXPECT_EQ(8, binary_search(ar, 9.0));
+}
+
+TEST(BinarySearch, FindValue) {
+	blitz::Array<double, 1> ar(10);
+	ar = 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0;
+	EXPECT_EQ(0, binary_search(ar, 0.01));
+	EXPECT_EQ(0, binary_search(ar, 0.99));
+	EXPECT_EQ(1, binary_search(ar, 1.01));
+	EXPECT_EQ(1, binary_search(ar, 1.99));
+	EXPECT_EQ(6, binary_search(ar, 6.1));
+	EXPECT_EQ(6, binary_search(ar, 6.5));
+	EXPECT_EQ(7, binary_search(ar, 7.2));
+	EXPECT_EQ(8, binary_search(ar, 8.01));
+	EXPECT_EQ(8, binary_search(ar, 8.99));
 }
