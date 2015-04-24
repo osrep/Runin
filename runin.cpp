9350666a7c71cf59c6aed98d06b988e9eded34d7
loczaj <loczaj@gmail.com>
2015-04-24 13:30:18 +0000

    Postprocessor workflow added

--- runin.cpp
+++ runin.cpp
@@ -32,7 +32,8 @@ void fire(ItmNs::Itm::coreprof &coreprof, ItmNs::Itm::coreimpur &coreimpur,
 
 void fire(ItmNs::Itm::coreprofArray &coreprof, ItmNs::Itm::coreimpurArray &coreimpur,
 		ItmNs::Itm::equilibriumArray &equilibrium, double &growth_rate_limit,
-		int &critical_field_warning, int &growth_rate_warning) {
+		int &critical_field_warning, int &growth_rate_warning, double &critical_field_time,
+		double &growth_rate_time) {
 
 	critical_field_warning = 0;
 	growth_rate_warning = 0;
@@ -46,21 +47,31 @@ void fire(ItmNs::Itm::coreprofArray &coreprof, ItmNs::Itm::coreimpurArray &corei
 			throw std::invalid_argument(
 					"Number of cpo slices is different in coreprof and equilibrium.");
 
-		for (int slice = 0; slice < slices; slice++) {
+		int slice = 0;
+		for (slice = 0; slice < slices; slice++) {
 
 			if (!equal(coreprof[slice].time, coreimpur[slice].time, 0.01)
 					|| !equal(coreprof[slice].time, equilibrium[slice].time, 0.01))
 				throw std::invalid_argument("Time value differs in cpo slices of the same index.");
 
-			if (critical_field_warning == 0)
+			if (critical_field_warning == 0) {
 				critical_field_warning = is_field_critical(
 						cpo_to_profile(coreprof[slice], coreimpur[slice], equilibrium[slice]));
 
-			if (growth_rate_warning == 0)
+				if (critical_field_warning != 0)
+					critical_field_time = coreprof[slice].time;
+			}
+
+			if (growth_rate_warning == 0) {
 				growth_rate_warning = is_growth_rate_over_limit(
 						cpo_to_profile(coreprof[slice], coreimpur[slice], equilibrium[slice]),
 						growth_rate_limit);
+
+				if (growth_rate_warning != 0)
+					growth_rate_time = coreprof[slice].time;
+			}
 		}
+
 	} catch (const std::exception& ex) {
 		std::cerr << "ERROR An error occurred during firing actor Runaway Indicator." << std::endl;
 		std::cerr << "ERROR : " << ex.what() << std::endl;
@@ -68,3 +79,4 @@ void fire(ItmNs::Itm::coreprofArray &coreprof, ItmNs::Itm::coreimpurArray &corei
 		growth_rate_warning = ITM_ILLEGAL_INT;
 	}
 }
+
