7f8de2d163b57579fb54594942274ab3f449df02
gergopokol <gergopokol@gmail.com>
2016-01-27 13:39:11 +0000

    Doxygen documentation completed

--- cpo_utils.cpp
+++ cpo_utils.cpp
@@ -1,9 +1,23 @@
 #include <stdexcept>
 #include "cpo_utils.h"
 
+/*!
+\param a
+\param b
+\param tolerance
+
+\details
+\f[
+	2 \cdot \left| a-b\right| \le \left( |a| + |b| \right) \cdot tolerance
+\f]
+*/
 bool equal(double a, double b, double tolerance) {
 	return abs(a - b) * 2.0 <= (abs(a) + abs(b)) * tolerance;
 }
+
+
+
+//! Binary search 
 
 int binary_search(const Array<double, 1> &array, int first, int last, double search_key) {
 	if (first == last)
@@ -26,6 +40,15 @@ int binary_search(const Array<double, 1> &array, double search_key) {
 	return binary_search(array, 0, array.rows() - 2, search_key);
 }
 
+
+/*!
+linear interpolation
+
+\f[
+	y_a = y_i + \frac{y_{i+1}-y_i}{x_{i+1}-x_i}\cdot(x_a -x_i)
+\f]
+
+*/
 double interpolate(const Array<double, 1> &x, const Array<double, 1> &y, double xa) {
 
 	int rows = x.rows();
@@ -44,12 +67,19 @@ double interpolate(const Array<double, 1> &x, const Array<double, 1> &y, double 
 	return y(index) + (y(index + 1) - y(index)) / (x(index + 1) - x(index)) * (xa - x(index));
 }
 
+
+
 profile cpo_to_profile(const ItmNs::Itm::coreprof &coreprof, const ItmNs::Itm::coreimpur &coreimpur,
 		const ItmNs::Itm::equilibrium &equilibrium) {
 
 	profile pro;
 
+	/*! read electron density profile
+	length of dataset: cells
+	*/
 	int cells = coreprof.ne.value.rows();
+	
+	//! read electron temperature profile
 	if (coreprof.te.value.rows() != cells)
 		throw std::invalid_argument("Number of values is different in coreprof ne and te.");
 	if (coreprof.profiles1d.eparallel.value.rows() != cells)
@@ -60,16 +90,24 @@ profile cpo_to_profile(const ItmNs::Itm::coreprof &coreprof, const ItmNs::Itm::c
 		cell celll;
 		celll.electron_density = coreprof.ne.value(rho);
 		celll.electron_temperature = coreprof.te.value(rho);
+		
+		/*! local electric field
+			\f[ E = \frac{E_\parallel(\rho) B_0}{B_\mathrm{av}(\rho)} \f]
+			where B_\mathrm{av} is known on discreate \f$R \f$ major radius and interpolated at $\rho$ major radius
+		*/
 		celll.electric_field = coreprof.profiles1d.eparallel.value(rho) * coreprof.toroid_field.b0
 				/ interpolate(equilibrium.profiles_1d.rho_tor, equilibrium.profiles_1d.b_av,
 						coreprof.rho_tor(rho));
 
+		//! counting ions in \a rho cell
 		celll.effective_charge = 0.0;
 		for (int ion = 0; ion < coreprof.compositions.ions.rows(); ion++) {
 			celll.effective_charge += coreprof.ni.value(rho, ion)
 					* coreprof.compositions.ions(ion).zion * coreprof.compositions.ions(ion).zion;
 		}
 
+
+		//! counting impurities in \a rho cell
 		for (int impurity = 0; impurity < coreimpur.impurity.rows(); impurity++) {
 
 			for (int ionization_degree = 0;
@@ -81,7 +119,10 @@ profile cpo_to_profile(const ItmNs::Itm::coreprof &coreprof, const ItmNs::Itm::c
 				Array<double, 1> charge_profile = coreimpur.impurity(impurity).z(Range::all(),
 						ionization_degree);
 
+				//! number of impurities by species
 				double nz = interpolate(coreimpur.rho_tor, density_profile, coreprof.rho_tor(rho));
+				
+				//! charge of impurity
 				double z = interpolate(coreimpur.rho_tor, charge_profile, coreprof.rho_tor(rho));
 
 				celll.effective_charge += nz * z * z;
