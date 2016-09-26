28a7bd578fb83e1b75f1db3a15d8ed824978f5f2
plasmatyi <aradimatyi@gmail.com>
2016-09-26 13:13:39 +0000

    [#810] CRITICAL BUG FIX: thermal electron collision time | code fix

--- growth_rate.h
+++ growth_rate.h
@@ -7,5 +7,7 @@ int is_growth_rate_over_limit(profile pro, double limit);
 
 double calculate_growth_rate(double electron_density, double electron_temperature,
 		double effective_charge, double electric_field);
+		
+double calculate_thermal_electron_collision_time(double electron_density, double electron_temperature);
 
 #endif /* GROWTH_RATE_H_ */
