7c8ed90ecd3b3183136965a131f4deb7671b7168
plasmatyi <aradimatyi@gmail.com>
2016-09-26 12:37:06 +0000

    [#810] CRITICAL BUG FIX: thermal electron collision time | pi_4_e02_me2__e4 added

--- growth_rate.cpp
+++ growth_rate.cpp
@@ -15,6 +15,8 @@ double me2_c3__e = me2_c3 / ITM_QE;
 double pi_4_e02_me2_c3__e4 = ITM_PI * 4.0 * pow(ITM_EPS0, 2) * me2_c3 / pow(ITM_QE, 4);
 
 
+//! 	\f[	\frac{4\pi\epsilon_0^2\cdot m_\mathrm{e}^2}{e^4} \f]
+double pi_4_e02_me2__e4  = ITM_PI * 4.0 * pow(ITM_EPS0, 2) * ITM_ME *ITM_ME  / pow(ITM_QE, 4);
 
 /*!
 
