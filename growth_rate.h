1100904db71a5e76948b51bcb9fb27549b04c338
Hadaradam <hadaradam20@gmail.com>
2017-05-09 13:39:20 +0000

    [#1194] Coulomb logarithm fix + refactoring | bug fix

--- growth_rate.h
+++ growth_rate.h
@@ -7,7 +7,9 @@ int is_growth_rate_over_limit(profile pro, double limit);
 
 double calculate_growth_rate(double electron_density, double electron_temperature,
 		double effective_charge, double electric_field);
-		
+	
+double calculate_coulomb_log(double electron_density, double electron_temperature);
+	
 double calculate_thermal_electron_collision_time(double electron_density, double electron_temperature);
 
 #endif /* GROWTH_RATE_H_ */
