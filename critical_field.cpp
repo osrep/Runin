6f4e6b00a396349af3026bdb8729e3c1089f0125
plasmatyi <aradimatyi@gmail.com>
2017-05-10 10:27:43 +0000

    [#1195] itmconstants products moved to products.h + bug fix for prev rev.

--- critical_field.cpp
+++ critical_field.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <itm_constants.h>
 #include "critical_field.h"
+#include "products.h"
 
 
 //! \f$ e^3 \f$
--- growth_rate.cpp
+++ growth_rate.cpp
@@ -4,22 +4,6 @@
 #include <itm_constants.h>
 #include "growth_rate.h"
 
-//! 	\f[	m_\mathrm{e} \cdot c^2 \f]
-double me_c2 = ITM_ME * pow(ITM_C, 2);
-
-//! 	\f[	m_\mathrm{e}^2 \cdot c^3 \f]
-double me2_c3 = me_c2 * ITM_ME * ITM_C;
-
-//! 	\f[	\frac{m_\mathrm{e}^2 \cdot c^3}{e} \f]
-double me2_c3__e = me2_c3 / ITM_QE;
-
-//! 	\f[	\frac{4\pi\epsilon_0^2\cdot m_\mathrm{e}^2 c^3}{e^4} \f]
-double pi_4_e02_me2_c3__e4 = ITM_PI * 4.0 * pow(ITM_EPS0, 2) * me2_c3 / pow(ITM_QE, 4);
-
-
-//! 	\f[	\frac{4\pi\epsilon_0^2\cdot m_\mathrm{e}^2}{e^4} \f]
-double pi_4_e02_me2__e4  = ITM_PI * 4.0 * pow(ITM_EPS0, 2) * ITM_ME *ITM_ME  / pow(ITM_QE, 4);
-
 /*!
 
 Growth rate warning
