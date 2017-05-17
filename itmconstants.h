9648bde46056f34b23e0e9f4263fbd28320c1ee1
plasmatyi <aradimatyi@gmail.com>
2017-05-17 12:48:52 +0000

    [#1202] itm_contansts added temporary to imas version | fixed

--- itmconstants.h
+++ itmconstants.h
@@ -1,8 +1,6 @@
 #ifndef CONSTANTS_H_
 #define CONSTANTS_H_
 
-#include <vector>
-
 //! value of \f$ \pi \f$
 #define ITM_PI		3.141592653589793238462643383280
 
@@ -21,22 +19,10 @@
 //! \f$ \mu_0 \f$ vacuum permeability
 #define ITM_MU0		4.0e-7 * ITM_PI
 
-
 //! \f$ \epsilon_0 \f$ vacuum permittivity
 #define ITM_EPS0	1.0 / (ITM_MU0 * ITM_C * ITM_C)
 
 //! ERROR CODE
 #define ITM_INVALID_INT	-999999999
-
-
-
-struct cell {
-	double electron_density;
-	double electron_temperature;
-	double effective_charge;
-	double electric_field;
-};
-
-typedef std::vector<cell> profile;
 
 #endif /* CONSTANTS_H_ */
