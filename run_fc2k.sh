f20a879c6c473a3673ae7d585acb62e302d00a9b
plasmatyi <aradimatyi@gmail.com>
2017-07-05 09:39:56 +0000

    [#1202] rmactor added to run_fc2k

--- run_fc2k.sh
+++ run_fc2k.sh
@@ -5,7 +5,12 @@
 # runaway indicator (EU-IM)
 
 if ($ITM_ENVIRONMENT_LOADED == yes) then
+
+    echo "*** Compiler set to ITM ***"
+    rmactor runaway_indicator
     fc2k -docfile doc/runaway_indicator.txt -codeversion 1.6.2 fc2k/runaway_indicator.xml 
 else
+    echo "*** Compiler set to IMAS ***"
+    rmactor runaway_indicator
     fc2k -docfile doc/runaway_indicator.txt -codeversion 1.6.2 fc2k_imas/runaway_indicator.xml 
-endif
+endiff
