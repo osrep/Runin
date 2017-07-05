c38084dc0486dc87a36a201f3d5b2c10b2fbc05a
plasmatyi <aradimatyi@gmail.com>
2017-07-05 09:51:07 +0000

    [#1202] IMAS preferences added to run_fc2k | bug fix

--- run_fc2k.sh
+++ run_fc2k.sh
@@ -4,13 +4,17 @@
 
 # runaway indicator (EU-IM)
 
-if ($ITM_ENVIRONMENT_LOADED == yes) then
-
+if (  ( $?ITM_ENVIRONMENT_LOADED) && $IMAS_ENVIRONMENT_LOADED == yes) then
     echo "*** Compiler set to ITM ***"
     rmactor runaway_indicator
     fc2k -docfile doc/runaway_indicator.txt -codeversion 1.6.2 fc2k/runaway_indicator.xml 
-else
+endif
+
+
+# runaway indicator (IMAS)
+
+if (  ( $?IMAS_ENVIRONMENT_LOADED) && $IMAS_ENVIRONMENT_LOADED == yes) then
     echo "*** Compiler set to IMAS ***"
     rmactor runaway_indicator
     fc2k -docfile doc/runaway_indicator.txt -codeversion 1.6.2 fc2k_imas/runaway_indicator.xml 
-endiff
+endif
