c3f20d8119eb605dcc34a64cad49b0c58560e2cd
plasmatyi <aradimatyi@gmail.com>
2017-07-31 17:15:45 +0000

    [#992] make file added to gtest | fix

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/tcsh -fe
 # This script starts unit tests of Runin.
 make --directory=$GTEST/make || exit
-cmake $GTEST || exit
+cmake -E chdir $GTEST make || exit
 make test
 chmod 755 test.bin || exit
 ./test.bin || exit
